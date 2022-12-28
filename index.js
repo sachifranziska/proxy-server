7dd9c6a3427596bc8beb1efc07fdffa551859e08
sachifranziska <s.shibata871@gmail.com>
2022-12-28 10:18:50 +0900

    indexファイル修正

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const rateLimit = require("express-rate-limit")
 require("dotenv").config()
-// const url = require("url")　URLのパラメーターを取得
+//URLのパラメーターを取得
+const url = require("url")
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -15,20 +16,30 @@ const limiter = rateLimit({
 
 // Getリクエスト、このコードのURL、ブラウザからのリクエスト、ブラウザへのレスポンス
 app.get("/", (req, res) => {
-  // console.log(req.url)
-  // const params = url.parse(req.url).query URLを文字列に変換、URLのパラメーターを取得
-  // console.log(params)
   res.send("This is my proxy server")
 })
 
 //個別のURLにのみ適用する場合は以下 
-app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
+
+// app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
+//   createProxyMiddleware({
+//     target: process.env.BASE_API_URL_CORONA_WORLD,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       [`^/corona-tracker-world-data`]: "",
+//     },
+//   })(req, res, next)
+// })
+
+app.use("/weather-data", (req, res, next) => {
+  // URLを文字列に変換、URLのパラメーターを取得
+  const city = url.parse(req.url).query
   createProxyMiddleware({
     // envファイルの情報を参照する（process）
-    target: process.env.BASE_API_URL_CORONA_WORLD,
+    target: `${process.env.BASE_API_URL_WEATHERAPI}${city}&api=no`,
     changeOrigin: true,
     pathRewrite: {
-      [`^/corona-tracker-world-data`]: "",
+      [`^/weather-data`]: "",
     },
   })(req, res, next)
 })
